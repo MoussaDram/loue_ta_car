45400b2cbc00c5e0a4ebe79f8621a65f3754481d
Moussa Drame <cooking33400@gmail.com>
2022-02-28 11:16:17 +0100

    photo

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -45,6 +45,6 @@ class CarsController < ApplicationController
   end
 
   def car_params
-    params.require(:car).permit(:brand, :model, :year, :address, :price, :description, :user_id)
+    params.require(:car).permit(:brand, :model, :year, :address, :price, :description, :user_id,:photo )
   end
 end
