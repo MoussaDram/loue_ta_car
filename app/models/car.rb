02392fd17e66c2c4cef1b866e8ee28bd6e6fb888
Moussa Drame <cooking33400@gmail.com>
2022-03-06 14:05:08 +0100

    install geocode

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,14 @@
 class Car < ApplicationRecord
+  validates :model, presence: true
+  validates :brand, presence: true
+  validates :year, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
